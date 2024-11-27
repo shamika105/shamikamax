47b65aa69e80a46aac45c2b528f5c20b3a37d270
Shamika_Diwan <91006618+shamika105@users.noreply.github.com>
2024-11-26 21:27:00 -0800

    Update max.py

--- shamika/max.py
+++ shamika/max.py
@@ -1,13 +1,9 @@
 #total numbers
-  total=[n]
-  for i in range(10):
+total=[n]
+for i in range(10):
 num = int (print("Enter Number:{1,n+1}"))
-
   numbers.append(num)
-
 #find max number of 10 numbers
-
   max_number = numbers
-
 #output the result
 print(f"The maximum no of 10 number is {max_number}:")
